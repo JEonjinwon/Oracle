b853ee1f1748d2e5d2f6c2342012cd5982440844
DESKTOP-1VE0JBC\PC-17 <wlsdnjs405@naver.com>
2021-02-19 18:12:15 +0900

    no message

---
+++ 0219.sql
@@ -0,0 +1,451 @@
+UPDATE LEC_PLAN
+         SET 
+         
+		    LEC_CODE ='5000601',
+		    LEC_WEEK =11,
+		    LEC_CONTENT ='11주차 강의 내용입니다.'
+        WHERE LEC_;
+        
+        
+        
+        
+        
+        
+        
+        
+          UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='14주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000101'
+		   		AND LEC_WEEK ='14'
+  ;
+  
+  
+  
+  
+       UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='14주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000101'
+		   	AND	 LEC_WEEK =14;
+            
+            
+            
+            ROLLBACK
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+ UPDATE LEC_PLAN SET LEC_CONTENT ='1주차 강의 내용입니다. XX' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =1 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='2주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =2 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='3주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =3 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='4주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =4 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='5주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =5 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='6주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =6 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='7주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =7 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='8주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =8 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='9주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =9 ; 
+; UPDATE LEC_PLAN SET LEC_CONTENT ='11주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =11 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='12주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =12 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='13주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =13 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='14주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =14 ; 
+UPDATE LEC_PLAN SET LEC_CONTENT ='15주차 강의 내용입니다.' WHERE LEC_CODE ='5000101' 
+AND LEC_WEEK =15 ;  
+
+
+
+
+
+
+  UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='15주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =15
+
+;
+UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='1주차 강의 내용입니다.ASD'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =1
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='2주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =2
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='3주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =3
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='4주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =4
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='5주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =5
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='6주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =6
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='7주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =7
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='8주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =8
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='9주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =9
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='10주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =10
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='11주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =11
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='12주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =12
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='13주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =13
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='14주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =14
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='15주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =15;
+
+;
+
+
+
+
+
+DECLARE BEGIN  
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='1주차 강의 내용입니다.asdzxcv'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =1
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='2주차 강의 내용입니다.zxz'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =2
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='3주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =3
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='4주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =4
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='5주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =5
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='6주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =6
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='7주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =7
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='8주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =8
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='9주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =9
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='10주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =10
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='11주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =11
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='12주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =12
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='13주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =13
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='14주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =14
+   
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='15주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =15
+ ; END;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ DECLARE BEGIN UPDATE LEC_PLAN SET LEC_CONTENT ='1주차 강의 내용입니다.asdf' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =1 ; UPDATE LEC_PLAN SET LEC_CONTENT ='2주차 강의 내용입니다.asdf' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =2 ; UPDATE LEC_PLAN SET LEC_CONTENT ='3주차 강의 내용입니다.as' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =3 ; UPDATE LEC_PLAN SET LEC_CONTENT ='4주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =4 ; UPDATE LEC_PLAN SET LEC_CONTENT ='5주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =5 ; UPDATE LEC_PLAN SET LEC_CONTENT ='6주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =6 ; UPDATE LEC_PLAN SET LEC_CONTENT ='7주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =7 ; UPDATE LEC_PLAN SET LEC_CONTENT ='8주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =8 ; UPDATE LEC_PLAN SET LEC_CONTENT ='9주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =9 ; UPDATE LEC_PLAN SET LEC_CONTENT ='10주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =10 ; UPDATE LEC_PLAN SET LEC_CONTENT ='11주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =11 ; UPDATE LEC_PLAN SET LEC_CONTENT ='12주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =12 ; UPDATE LEC_PLAN SET LEC_CONTENT ='13주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =13 ; UPDATE LEC_PLAN SET LEC_CONTENT ='14주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =14 ; UPDATE LEC_PLAN SET LEC_CONTENT ='15주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =15 ; END;
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='1주차 강의 내용입니다.xzcv'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =1
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='2주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =2
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='3주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =3
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='4주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =4
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='5주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =5
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='6주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =6
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='7주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =7
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='8주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =8
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='9주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =9
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='10주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =10
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='11주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =11
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='12주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =12
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='13주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =13
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='14주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =14
+ ; 
+        UPDATE LEC_PLAN
+         SET 
+		    LEC_CONTENT ='15주차 강의 내용입니다.'
+		 WHERE   LEC_CODE ='5000102'
+		   	AND	 LEC_WEEK =15
+ ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+DECLARE BEGIN UPDATE LEC_PLAN SET LEC_CONTENT ='1주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =1 ; UPDATE LEC_PLAN SET LEC_CONTENT ='2주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =2 ; UPDATE LEC_PLAN SET LEC_CONTENT ='3주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =3 ; UPDATE LEC_PLAN SET LEC_CONTENT ='4주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =4 ; UPDATE LEC_PLAN SET LEC_CONTENT ='5주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =5 ; UPDATE LEC_PLAN SET LEC_CONTENT ='6주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =6 ; UPDATE LEC_PLAN SET LEC_CONTENT ='7주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =7 ; UPDATE LEC_PLAN SET LEC_CONTENT ='8주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =8 ; UPDATE LEC_PLAN SET LEC_CONTENT ='9주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =9 ; UPDATE LEC_PLAN SET LEC_CONTENT ='10주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' AND 
+LEC_WEEK =10 ; UPDATE LEC_PLAN SET LEC_CONTENT ='11주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =11 ; UPDATE LEC_PLAN SET LEC_CONTENT ='12주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =12 ; UPDATE LEC_PLAN SET LEC_CONTENT ='13주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =13 ; UPDATE LEC_PLAN SET LEC_CONTENT ='14주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =14 ; UPDATE LEC_PLAN SET LEC_CONTENT ='15주차 강의 내용입니다.' WHERE LEC_CODE ='5000102' 
+AND LEC_WEEK =15 ; END; 
+
+
+
+SELECT LEC_WEEK,
+        LEC_CONTENT
+FROM LEC_PLAN
+WHERE LEC_CODE= ( SELECT LEC_CODE FROM LECTURE WHERE SUB_CODE ='50001');
+
+
+ SELECT B.SUB_NAME, B.SUB_CODE, A.LEC_CODE
+ FROM LECTURE A, SUBJECT B  
+ WHERE MEM_ID = '510001'
+ AND A.SUB_CODE = B.SUB_CODE
+ AND A.LEC_SMST =  (SELECT CODE_NAME FROM CODE WHERE GROUP_CODE = 'A00');
+
+
+
+SELECT LEC_WEEK, LEC_CONTENT FROM LEC_PLAN WHERE LEC_CODE='5000102' order by 1
+
+rollback
+
+
+
